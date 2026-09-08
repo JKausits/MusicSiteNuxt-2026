1722167a1bcd2a5017e46896600143ab5aa3c219
Joe Kausits <joe.kausits@amwins.com>
2026-09-08 16:57:20 -0400

    Update shows

--- app/stores/upcoming-shows.store.ts
+++ app/stores/upcoming-shows.store.ts
@@ -8,39 +8,21 @@ export const useUpcomingShowsStore = defineStore("upcoming-shows", () => {
     // TODO: API Call.
     shows.value = [
       {
-        id: "1",
-        startAt: new Date(2026, 9, 5, 9, 0, 0, 0),
-        endAt: new Date(2026, 9, 5, 13, 0, 0),
-        venue: { id: "1", name: "Howland Farmer's Market" },
-      },
-      {
-        id: "2",
-        startAt: new Date(2026, 9, 6, 17, 0, 0, 0),
-        endAt: new Date(2026, 9, 6, 20, 0, 0),
-        venue: { id: "2", name: "Kennsington" },
-      },
-      {
-        id: "3",
-        startAt: new Date(2026, 9, 7, 16, 0, 0, 0),
-        endAt: new Date(2026, 9, 7, 18, 0, 0),
-        venue: { id: "3", name: "Canfield Fair Art and Photo Building" },
-      },
-      {
         id: "4",
-        startAt: new Date(2026, 9, 11, 18, 0, 0, 0),
-        endAt: new Date(2026, 9, 11, 21, 0, 0),
+        startAt: new Date(2026, 8, 11, 18, 0, 0, 0),
+        endAt: new Date(2026, 8, 11, 21, 0, 0),
         venue: { id: "4", name: "Good Lady Brewhouse" },
       },
       {
         id: "5",
-        startAt: new Date(2026, 9, 18, 19, 30, 0, 0),
-        endAt: new Date(2026, 9, 18, 23, 30, 0),
+        startAt: new Date(2026, 8, 18, 19, 30, 0, 0),
+        endAt: new Date(2026, 8, 18, 23, 30, 0),
         venue: { id: "4", name: "The Dessert Bar at Gorant" },
       },
       {
         id: "6",
-        startAt: new Date(2026, 9, 26, 18, 0, 0, 0),
-        endAt: new Date(2026, 9, 26, 21, 0, 0),
+        startAt: new Date(2026, 8, 26, 18, 0, 0, 0),
+        endAt: new Date(2026, 8, 26, 21, 0, 0),
         venue: { id: "4", name: "Greene Eagle Winery" },
       },
     ].filter((x) => x.endAt > currentDate);
